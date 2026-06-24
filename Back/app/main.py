c035be410eb25ffc2b6ad5e0ac3aafa69c616161
NABIL\nabil <nabiliomg345@gmail.com>
2026-06-24 12:36:56 +0200

    arreglado cors

--- Back/app/main.py
+++ Back/app/main.py
@@ -8,7 +8,17 @@ load_dotenv()
 
 app = FastAPI()
 
-app.add_middleware(CORSMiddleware,allow_origins=["http://localhost:5173"],allow_credentials=True,allow_methods=["*"],allow_headers=["*"],)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:5173",
+        "https://generadorcv.vercel.app",
+        "https://generador-cv-six.vercel.app"
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.include_router(registro.app, prefix="/api/auth", tags=["auth"])
 app.include_router(cv.app, prefix="/api", tags=["cv"])
 
